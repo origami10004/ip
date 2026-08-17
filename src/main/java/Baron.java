49d50d0d0efd59aac2d864e10250b8656e95e5ee
origami100 <origami10004@gmail.com>
2026-08-17 14:38:40 +0800

    Add greeting

--- src/main/java/Baron.java
+++ src/main/java/Baron.java
@@ -1,11 +1,31 @@
 public class Baron {
+    public static final String NAME = "Baron";
     public static void main(String[] args) {
-        String banner = "__________\n"
+        greet();
+
+    }
+
+
+    private static String banner() {
+        return "__________\n"
             + "\\______   \\_____ _______  ____   ____\n"
             + " |    |  _/\\__  \\\\_  __ \\/  _ \\ /    \\\n"
             + " |    |   \\ / __ \\|  | \\(  <_> )   |  \\\n"
             + " |______  /(____  /__|   \\____/|___|  /\n"
-            + "        \\/      \\/                  \\/"
-        System.out.println(banner);
+            + "        \\/      \\/                  \\/";
+    }
+
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void greet() {
+        printLine();
+        System.out.println(banner());
+        System.out.println("Hello! I'm " + NAME + ".\nWhat can I do for you?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+        
     }
 }
