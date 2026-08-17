ac8ce20d54d6c5e4b1289fe04612e7dff4f00f2f
Origami10004 <42462409+origami10004@users.noreply.github.com>
2026-08-17 14:26:14 +0800

    Demoted bot to Baron

---
+++ src/main/java/Baron.java
@@ -0,0 +1,11 @@
+public class Baron {
+    public static void main(String[] args) {
+        String banner = "__________\n"
+            + "\\______   \\_____ _______  ____   ____\n"
+            + " |    |  _/\\__  \\\\_  __ \\/  _ \\ /    \\\n"
+            + " |    |   \\ / __ \\|  | \\(  <_> )   |  \\\n"
+            + " |______  /(____  /__|   \\____/|___|  /\n"
+            + "        \\/      \\/                  \\/"
+        System.out.println(banner);
+    }
+}
